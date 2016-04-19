25fe82c390fa5cf5a4543759cc215ee345fd5495
Geoff Kimball <gakimball@bsu.edu>
2016-04-19 14:50:58 -0700

    Add recursive parsing, and add placeholder CSS to show tree structure of JSON

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const HTML = `
 <!doctype html>
 <html>
   <head>
+    <style>
+      .group,
+      .value {
+        padding: 0.5rem;
+      }
+
+      .group {
+        border: 1px solid red;
+      }
+
+      .value {
+        border: 1px solid blue;
+        margin-bottom: 0.5rem;
+      }
+    </style>
   </head>
   <body>
-    <pre>%s</pre>
+    %s
   </body>
 </html>
 `;
@@ -19,11 +34,22 @@ module.exports = module.exports.parse = parse;
 
 /**
  * Generates an HTML page from a JSON object.
- * @param {object} json - Input JSON.
+ * @param {object} value - Input JSON.
  * @returns {string} HTML page displaying the JSON.
  */
-function parse(json) {
-  return format(HTML, JSON.stringify(json, null, '  '));
+function parse(value) {
+  let output = '';
+
+  for (let i in value) {
+    output += process(i, value[i]);
+  }
+
+  return format(HTML, `
+    <div class="group">
+      <strong>root</strong>
+      ${output}
+    </div>
+  `);
 }
 
 /**
@@ -35,4 +61,29 @@ module.exports.open = function open(json) {
   return temp(output).then(path => {
     opn(path);
   });
+}
+
+function process(key, value) {
+  let newValue = '';
+
+  // Recursively process arrays and objects
+  if (value !== null && (typeof value === 'object' || Array.isArray(value))) {
+    let inner = '';
+
+    for (let i in value) {
+      inner += process(i, value[i]);
+    }
+
+    newValue = `<div class="group">${inner}</div>`;
+  }
+  else {
+    newValue = value;
+  }
+
+  return `
+    <div class="value">
+      <strong>${key}</strong>
+      ${newValue}
+    </div>
+  `;
 }
